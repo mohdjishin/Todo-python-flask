d23fb3de8e29302b437d22e849ea2d3d5beee443
mohdjishin <whoistcp@protonmail.com>
2022-04-26 02:58:51 +0530

    insert done

--- app.py
+++ app.py
@@ -20,11 +20,29 @@ app.secret_key = b'_325523ewvrX?'
 
 
 
-@app.route('/')
+@app.route('/',methods=['GET','POST'])
 def index():
-    
-    mycursor.execute("SELECT * FROM list")
-    data = mycursor.fetchall()
+    if request.method == 'GET':
+        mycursor.execute("SELECT * FROM list")
+        data = mycursor.fetchall()
+    else:
+        msg = request.form['inputmg']
+        sql = 'INSERT INTO list (msg) VALUES(%s);'
+        val = (msg,)
+        
+        mycursor.execute(sql,val)
+        mydb.commit()
+        if mycursor.rowcount > 0:
+            message = "Insert Success"
+        else:
+            message = None
+        flash(message)
+        return redirect(url_for('index'))
+        
+        
+        
+        
+        return msg
     
     
     return render_template('table.html',data = data)
